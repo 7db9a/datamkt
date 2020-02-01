21245430fe793b7c8e92bd9511abe12bfd47ba71
7db9a <repo.7db9a@gmail.com>
2020-01-31 22:40:00 -0500

    feat: create sub.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,54 @@
 use eosio::*;
 use eosio_cdt::*;
 use PrimaryTableCursor as ptc;
+
+#[eosio::action]
+fn createsub(
+    owner: AccountName,
+    sub_name: String,
+) {
+
+    require_auth(owner);
+
+    let _self = current_receiver();
+    let substbl = subprofile::table(_self, _self);
+
+    // Check if the owner already has a sub of the same name.
+    let sub_count = substbl.iter().filter_map(|x| x.get().ok())
+                             .filter(|x| x.owner == owner)
+                             .filter(|x| x.sub_name == sub_name)
+                             .count();
+    check(sub_count == 0, "sub name already exists");
+
+    let sub_id = substbl.available_primary_key().expect("failed to get primary key");
+
+
+    let sub = subprofile {
+        sub_id,
+        sub_name,
+        owner,
+    };
+
+    substbl.emplace(owner, &sub).check("write");
+}
+
+eosio_cdt::abi!(createrepo);
+
+#[eosio::table("repoprofile")]
+struct Subprofile {
+ #[eosio(primary_key)]
+ sub_id: u64,
+ sub_name: String,
+ owner: AccountName,
+}
+
+#[eosio::table("sub")]
+struct Sub {
+ #[eosio(primary_key)]
+ file_id: u64,
+ file_name: String,
+ ipfs_hash: String,
+ #[eosio(secondary_key)]
+ sub_id: u64,
+ last_contributor: AccountName,
+}
